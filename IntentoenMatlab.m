1b46487cebfb5940e57611d9bea5de537a8a9ee1
Carrillo-coder <151704695+Carrillo-coder@users.noreply.github.com>
2024-05-02 14:15:35 -0600

    Grafica el potencial e input
    Grafica el potencial y crea un cuadro de dialogo para el input del user

--- IntentoenMatlab.m
+++ IntentoenMatlab.m
@@ -1,41 +1,81 @@
 clear;
 clc;
-x = linspace(0,6,100);
-y = linspace(-3,3,100);
+%inputs para el usuario
+
+prompt = {'Ingresa la longitud:','Ingresa el punto incial en Y:', 'Ingresa la posicion en X:','Ingresa la carga:'};
+dlgtitle = 'Varilla 1';
+fieldsize = [1 45];
+definput = {'4','-2','2','1'};
+Varilla1 = inputdlg(prompt,dlgtitle,fieldsize,definput);
+
+prompt = {'Ingresa la longitud:','Ingresa el punto incial en Y:', 'Ingresa la posicion en X:','Ingresa la carga:'};
+dlgtitle2 = 'Varilla 2';
+fieldsize = [1 45];
+definput2 = {'2','-1','3','-1'};
+Varilla2 = inputdlg(prompt,dlgtitle2,fieldsize,definput2);
+
+%parametros varilla 1
+q=str2double(Varilla1{4});
+lim_inf=str2double(Varilla1{2});
+longitud=str2double(Varilla1{1});
+lim_sup=lim_inf + longitud;
+xv=str2double(Varilla1{3});
+
+%Parametros varilla 2
+q2=str2double(Varilla2{4});
+lim_inf2=str2double(Varilla2{2});
+longitud2=str2double(Varilla2{1});
+lim_sup2=lim_inf2 + longitud2;
+xv2=str2double(Varilla2{3});
+
+%Creación del espacio y constantes
+x = linspace(min([xv,xv2])-2,max([xv,xv2])+2,100);
+y = linspace(min([lim_inf,lim_inf2])-1,max([lim_sup,lim_sup2])+1,100);
 k = 9e9;
 [X, Y] = meshgrid(x, y);
-q=1;
-limite_superior=2;
-limite_inferior=-2;
-longitud=4;
-xv=2;
 
-funcion_campo_en_x=@(x,y) ((-k.*q.*(y-limite_superior))./(longitud.*(x-xv).*sqrt(((x-xv).^2)+((y-limite_superior).^2))))-((-k.*q.*(y-limite_inferior))./(longitud.*(x-xv).*sqrt(((x-xv).^2)+((y-limite_inferior).^2))));
-funcion_campo_en_y=@(x,y) ((k.*q)./(longitud.*(sqrt(((x-xv).^2)+((y-limite_superior).^2)))))-((k.*q)./(longitud.*(sqrt(((x-xv).^2)+((y-limite_inferior).^2)))));
-funcion_Voltaje = @(x,y) (k.*q.*(x-xv))./(((x-xv).^2 + (y-longitud).^2).^(3/2));
+%Calculos para varilla 1
+FcX=@(x,y) ((-k.*q.*(y-lim_sup))./(longitud.*(x-xv).*sqrt(((x-xv).^2)+((y-lim_sup).^2))))-((-k.*q.*(y-lim_inf))./(longitud.*(x-xv).*sqrt(((x-xv).^2)+((y-lim_inf).^2))));
+FcY=@(x,y) ((k.*q)./(longitud.*(sqrt(((x-xv).^2)+((y-lim_sup).^2)))))-((k.*q)./(longitud.*(sqrt(((x-xv).^2)+((y-lim_inf).^2)))));
 
-Ey=funcion_campo_en_y(X,Y);
-Ex=funcion_campo_en_x(X,Y);
-V = funcion_Voltaje(X,Y);
+% for i = lim_inf:0.01:lim_sip
+%     Rn = sqrt((X-xv).^2+(Y-i).^2);
+%     Vn = k+(q./longitud)./Rn;
+%     v1 = Vn + v1;
+% end
+v1 = Calculo_voltaje(X,Y,lim_sup,lim_inf,longitud,k,q,xv);
+Ey=FcY(X,Y);
+Ex=FcX(X,Y);
 
-q2=-1;
-limite_superior2=1;
-limite_inferior2=-1;
-longitud2=2;
-xv2=3;
+%Calculos varilla 2
+FcX2=@(x,y) ((-k.*q2.*(y-lim_sup2))./(longitud2.*(x-xv2).*sqrt(((x-xv2).^2)+((y-lim_sup2).^2))))-((-k.*q2.*(y-lim_inf2))./(longitud2.*(x-xv2).*sqrt(((x-xv2).^2)+((y-lim_inf2).^2))));
+FcY2=@(x,y) ((k.*q2)./(longitud2.*(sqrt(((x-xv2).^2)+((y-lim_sup2).^2)))))-((k.*q2)./(longitud2.*(sqrt(((x-xv2).^2)+((y-lim_inf2).^2)))));
 
-funcion_campo_en_x2=@(x,y) ((-k.*q2.*(y-limite_superior2))./(longitud2.*(x-xv2).*sqrt(((x-xv2).^2)+((y-limite_superior2).^2))))-((-k.*q2.*(y-limite_inferior2))./(longitud2.*(x-xv2).*sqrt(((x-xv2).^2)+((y-limite_inferior2).^2))));
-funcion_campo_en_y2=@(x,y) ((k.*q2)./(longitud2.*(sqrt(((x-xv2).^2)+((y-limite_superior2).^2)))))-((k.*q2)./(longitud2.*(sqrt(((x-xv2).^2)+((y-limite_inferior2).^2)))));
+% for i = lim_inf2:0.01:lim_sup2
+%     Rn2 = sqrt((X-xv2).^2+(Y-i).^2);
+%     Vn2 = k+(q2./longitud)./Rn2;
+%     v2 = Vn2 + v2;
+% end
 
-Ey2=funcion_campo_en_y2(X,Y);
-Ex2=funcion_campo_en_x2(X,Y);
+v2 = Calculo_voltaje(X,Y,lim_sup2,lim_inf2,longitud,k,q2,xv2);
+Ey2=FcY2(X,Y);
+Ex2=FcX2(X,Y);
 
+%Calculos de superposición
 Eyt=Ey+Ey2;
 Ext=Ex+Ex2;
+V = v1 + v2;
 
+% Graficar las flechas con streamslice
 figure;
-% Graficar las flechas con quiver
 streamslice(X, Y, Ext, Eyt);
-title('Campo Electrico de una Carga Puntual')
+hold on
+contour(X,Y,V,100)
+axis equal
+title('Campo Electrico de 2 placas')
 xlabel('x (m)')
 ylabel('y (m)')
+
+function [voltaje] = Calculo_voltaje(x,y,lim_sip,lim_inf,longitud,k,q,xv)
+    voltaje=((k.*q)./longitud).*log(abs((sqrt(((x-xv).^2)+((y-lim_sip).^2))+(y-lim_sip))./(x-xv)))-((k.*q)./longitud).*log(abs((sqrt(((x-xv).^2)+((y-lim_inf).^2))+(y-lim_inf))./(x-xv)));
+end
